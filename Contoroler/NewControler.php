593970708c47fe542f2e01014447fd93f00d85d4
Alexandr <alex007_97@mail.ru>
2017-06-17 12:34:32 +0700

    Полностью релизовал ORM

--- Contoroler/NewControler.php
+++ Contoroler/NewControler.php
@@ -7,16 +7,30 @@
  * Time: 13:41
  */
 require_once __DIR__ . "/../Models/Person.php";
+require_once __DIR__ . "/../Models/NewModel.php";
+//echo  __DIR__ . "/../Models/Person.php";
 class NewControler
 {
 
 public function actionAll()
 {
-    $items=Person::GetAll();
+    $person=new NewModel();
+    $person->data['name']='Ренат';
+    $person->data['phone']="55555";
+    $person->save();
+   // $person->findOnBycolum(3,"id");
+    echo $person->id;
+  // echo NewModel::getTable();
+    /*  $items=Person::GetAll();
     $view=new View();
-    $view->assing("items",$items);
+   // $view->assing("items",$items);
+     $view->items=$items;
+    //echo count($view);
+
+  //  $view->display("news/all.php");
     $view->display("news/all.php");
-    //require_once __DIR__ . "/../views/news/all.php";
+    hui!
+    //require_once __DIR__ . "/../views/news/all.php";*/
 }
 
 public function actionOne()
